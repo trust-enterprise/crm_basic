cabdec8cbfe51bc0e7e877d8a30bf67247e22228
Sheikh Abdullah <sheikhabdullah.aps@gmail.com>
2026-06-18 06:12:50 +0530

    recommitting

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("Home page")
+
+
+def contact(request):
+    return HttpResponse("Contact page")
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", home),
+    path("about/", contact),
+]
