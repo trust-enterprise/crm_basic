e9a2a44f8ac878ddfbb876c648716e0265480000
Sheikh Abdullah <sheikhabdullah.aps@gmail.com>
2026-06-18 06:12:17 +0530

    learning HttpResponse

--- crm_basic/urls.py
+++ crm_basic/urls.py
@@ -18,6 +18,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("Home page")
+
+
+def contact(request):
+    return HttpResponse("Contact page")
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", home),
+    path("about/", contact),
 ]
